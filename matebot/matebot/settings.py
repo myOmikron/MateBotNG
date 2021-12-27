d11b46ecc4ab112367cb2a6f95c22359242853d9
myOmikron <accounts@omikron.dev>
2021-12-27 10:11:17 +0100

    Removed rcp from settings as it is now saved seperatly for each application

--- matebot/matebot/settings.py
+++ matebot/matebot/settings.py
@@ -22,8 +22,6 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # SECURITY WARNING: keep the secret key used in production secret!
 SECRET_KEY = 'change_me'
 
-RCP_SECRET = 'change_me'
-
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
