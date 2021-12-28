e9e392889117d76388520e242ecb3544a55a9d71
myOmikron <accounts@omikron.dev>
2021-12-28 21:27:47 +0100

    Added admin views

--- matebot/api/admin.py
+++ matebot/api/admin.py
@@ -1,3 +1,58 @@
 from django.contrib import admin
 
-# Register your models here.
+from api.models import *
+
+
+@admin.register(ApplicationModel)
+class ApplicationAdmin(admin.ModelAdmin):
+    pass
+
+
+@admin.register(UserModel)
+class UserAdmin(admin.ModelAdmin):
+    pass
+
+
+@admin.register(VoteModel)
+class VoteAdmin(admin.ModelAdmin):
+    pass
+
+
+@admin.register(UserAliasModel)
+class UserAliasAdmin(admin.ModelAdmin):
+    pass
+
+
+@admin.register(ConsumableMessageModel)
+class ConsumableMessageAdmin(admin.ModelAdmin):
+    pass
+
+
+@admin.register(ConsumableModel)
+class ConsumableAdmin(admin.ModelAdmin):
+    pass
+
+
+@admin.register(CommunismModel)
+class CommunismAdmin(admin.ModelAdmin):
+    pass
+
+
+@admin.register(CommunismUserModel)
+class CommunismUserAdmin(admin.ModelAdmin):
+    pass
+
+
+@admin.register(TransactionModel)
+class TransactionAdmin(admin.ModelAdmin):
+    pass
+
+
+@admin.register(MembershipPollModel)
+class MembershipPollAdmin(admin.ModelAdmin):
+    pass
+
+
+@admin.register(RefundModel)
+class RefundAdmin(admin.ModelAdmin):
+    pass
